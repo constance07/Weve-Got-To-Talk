9f6e6ce1dc3b46d63e31030ae0d98e4ad5346169
Constance <116240430+constance07@users.noreply.github.com>
2025-07-02 10:28:28 -0400

    Day 18: About Page Complete

--- wntt/app/components/footer.tsx
+++ wntt/app/components/footer.tsx
@@ -5,27 +5,26 @@ import { Be_Vietnam_Pro} from 'next/font/google';
 {/*VBold*/}const be_Vietnam_Pro = Be_Vietnam_Pro({subsets: ['latin'], weight: '800'})
 {/*Light*/}const be_Vietnam_Pro_Light = Be_Vietnam_Pro({subsets: ['latin'], weight: '300'})
 {/*VLight*/}const be_Vietnam_Pro_VLight = Be_Vietnam_Pro({subsets: ['latin'], weight: '200'})
+{/*General Imports*/}
+import Image from 'next/image';
+import blankProfile from '../images/blankProfile.jpg';
 
 export default function Footer(){
     return(
         <>
             <div className='footerContainer'>
+                {/*Footer Image*/}
+                <Image src = {blankProfile}  width = {60} height = {60} alt = 'Picture of Brand Logo' quality={100} className='logoContainer'></Image>
                 {/*Footer Title*/}
                 <div className='footerTitle'>
                     <div className={be_Vietnam_Pro.className}>
-                        SHARE YOURE THOUGHTS
+                        © 2025 THE HOUSE OF SOFTWARE. All rights reserved.
                     </div>
                 </div>
                 {/*Footer Subtitle*/}
                 <div className='footerSubTitle'>
                     <div className= {be_Vietnam_Pro_VLight.className}>
-                        I’d love to hear your feedback:
-                    </div>
-                </div>
-                {/*Email*/}
-                <div className='footerEmail'>
-                    <div className= {be_Vietnam_Pro_VLight.className}>
-                        Serenc0107@gmail.com
+                        Created so you aren't a stranger to yourself.
                     </div>
                 </div>
             </div>
