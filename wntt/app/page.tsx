9d587a2ea27d664e8cc49227e6bf1860b4b29fd4
Serenity Buckner <Serenc0107@gmail.com>
2025-09-09 18:17:58 -0400

    Day 24(B): Fixed ESLint Error

--- wntt/app/page.tsx
+++ wntt/app/page.tsx
@@ -11,12 +11,7 @@ import { Be_Vietnam_Pro} from 'next/font/google';
 
 
 export default function Home() {
-<link
-  rel="icon"
-  href= '@/app/images/logo.png'
-  type="image/<generated>"
-  sizes="<generated>"
-/>
+
   return (
     <div className = {be_Vietnam_Pro.className}>
       <Navigation/>
